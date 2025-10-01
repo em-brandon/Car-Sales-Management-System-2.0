35f974e92218a6eceddb510c98e63456c4c9d165
em-brandon <brandonmwavua@gmail.com>
2025-10-01 13:24:34 +0300

    2.1

---
+++ src/app/(presentation)/inventory/classified-card.tsx
@@ -0,0 +1,8 @@
+export const ClassifiedCard = (props: any) => {
+import Link from "next/link
+
+return <divclassName= "aspect -3/2 relative ">
+
+<Link href = {routes.singleClassified("slug")}></Link>
+}
+
