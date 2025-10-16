7497dfb0ce3dc24f473e0b092ba891d7432c250a
em-brandon <brandonmwavua@gmail.com>
2025-10-16 16:05:27 +0300

    3.1

--- prisma/classifieds.seed.ts
+++ prisma/classifieds.seed.ts
@@ -1,5 +1,6 @@
-import type { Prisma,  PrismaClient } from "@prisma/client";
+
 import { faker } from '@faker-js/faker'
+import slugify from 'slug';
 
 export async function seedClassifieds(prisma: PrismaClient ) {
     const makes = await prisma.make.findMany({
@@ -24,7 +25,27 @@ for ( let i = 0; i < 25 ; i++) {
 
     console.log({make, model, variants});
 
-}
+    const year = faker.date.between({
+        
+        from: new Date (1925, 0 , 1 )
+        to: new Date(),
+    }).getFullYear();
 
-}
+    const tiltle = [year, make.name, model.name, variants?.name].filter(Boolean).join (' ');
 
+    const vrm = faker.vehicle.vrm();
+
+    const baseSlug = slugify(`${tiltle} ${vrm}`);
+
+    classifiedsData.push({
+        year,
+        vrm,
+        slug: baseSlug,
+        makeId: make.id,
+        modelId: model.id,
+        ... (variants?.id && {modelVariantId: variants.id}),
+        title,
+        price: faker.number.int ({min: 400000, max: 10000000}),
+        currency: CurrencyCode.USD,
+        odoUnit: faker.helpers.arrayElement(Object.values(OdoUnit)),
+        
